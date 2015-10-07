49c91977c329300dd38e6d489ea07fb03b657b1c
James-Thorson <JamesT.esq@gmail.com>
2015-10-07 08:42:32 -0700

    add new function to rename column labels

---
+++ R/rename_columns.R
@@ -0,0 +1,8 @@
+rename_columns = function( DF, origname, newname ){
+  DF_new = DF
+  for(i in 1:length(origname)){
+    Match = match( origname[i], colnames(DF_new) )
+    if(length(Match)==1) colnames(DF_new)[Match] = newname[i]
+  }
+  return(DF_new)
+}
