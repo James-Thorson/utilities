1db160cc5b88ed654f22643c72e2cae8c6ca3fe5
Jim Thorson <JamesT.esq@gmail.com>
2015-12-03 08:23:53 -0800

    added functions for phylogenetic trees

---
+++ R/JoinTree_Fn.R
@@ -0,0 +1,30 @@
+
+JoinTree_Fn = function( EdgeSet, Nodes2Keep, EdgeDist){
+  EdgeSetOrig = EdgeSet
+  DistOrig = EdgeDist
+  # Figure out which are important (i.e. nodes with two outputs, or terminal nodes)
+  n_out = table(EdgeSetOrig[,1])
+  ImportantNodes = union( EdgeSetOrig[Nodes2Keep,2], as.numeric(names(n_out)[which(n_out>=2)]) )
+  # Identify branches to start with when pruning
+  Rows_to_start = which( EdgeSetOrig[,1]  %in% ImportantNodes)
+  # New edge and distance set
+  EdgeSetNew = DistNew = NULL
+  # populate new edge set and dist set
+  for(i in 1:length(Rows_to_start)){
+    EdgeSetNew = rbind( EdgeSetNew, EdgeSetOrig[Rows_to_start[i],] )
+    DistNew = c( DistNew, DistOrig[Rows_to_start[i]] )
+    while( TRUE ){
+      # Exit if at important node
+      if( EdgeSetNew[i,2] %in% ImportantNodes ) break()
+      # IDentify row and check for error
+      RowI = which( EdgeSetOrig[,1] == EdgeSetNew[i,2] )
+      if( length(RowI)>=2 ) stop("Some problem")
+      # Join new branch to starting branch
+      EdgeSetNew[i,2] = EdgeSetOrig[RowI,2]
+      DistNew[i] = DistNew[i] + DistOrig[RowI]
+    }
+  }
+  # return
+  Return = list("ImportantNodes"=ImportantNodes, "EdgeSetNew"=EdgeSetNew, "DistNew"=DistNew )
+  return(Return)
+}
---
+++ R/PruneTree_Fn.R
@@ -0,0 +1,27 @@
+
+PruneTree_Fn = function( EdgeSet, Nodes2Keep, EdgeDist ){
+  # IDEA: if network is "built" from single node, then there's one single path from outer ("starting") nodes to original ("terminal") node
+  # Starting nodes
+  Nodes2Match = Nodes2Keep
+  # Aggregator for rows in tree that are necessary
+  Rows2Keep = NULL
+  # Total distance for each starting node to terminal node
+  TotalDist = rep(0,length(Nodes2Keep))
+  # Number of times each element in distance vector is used
+  n_times_row_used = rep(0,nrow(EdgeSet))
+  # path for each starting node
+  Path2TopNode = vector("list",length=length(Nodes2Keep))
+  # Loop through network from "starting" to "terminal" node
+  while( length(na.omit(Nodes2Match))>0 ){
+    NewRows = match(Nodes2Match, EdgeSet[,2])
+    Rows2Keep = union(Rows2Keep, na.omit(NewRows))
+    Nodes2Match = EdgeSet[NewRows,1]
+    NewDist = EdgeDist[NewRows]
+    TotalDist = TotalDist + ifelse(is.na(NewDist), 0, NewDist)
+    n_times_row_used[na.omit(NewRows)] = n_times_row_used[na.omit(NewRows)] + 1
+    for(i in 1:length(Nodes2Keep)) if( !is.na(NewRows[i]) ) Path2TopNode[[i]] = c( Path2TopNode[[i]], NewRows[i])
+  }
+  # Return stuff
+  Return = list( "Rows2Keep"=Rows2Keep, "Path2TopNode"=Path2TopNode )
+  return( Return )
+}
