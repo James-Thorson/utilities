c9ad234d95a308fc83cf1c22a89ccfa6f5ae219c
Jim Thorson <JamesT.esq@gmail.com>
2015-12-17 15:03:08 -0800

    new, faster method for adding zeros

--- R/add_missing_zeros.R
+++ R/add_missing_zeros.R
@@ -1,5 +1,5 @@
 
-add_missing_zeros = function( data_frame, unique_sample_ID_colname, sample_colname, species_subset=NULL, species_colname, if_multiple_records="Error", verbose=TRUE, na.rm=FALSE, save_name=NULL ){
+add_missing_zeros = function( data_frame, unique_sample_ID_colname, sample_colname, species_subset=NULL, species_colname, if_multiple_records="Error", Method="Slow", verbose=TRUE, na.rm=FALSE, save_name=NULL ){
 
   if( is.null(save_name) || !file.exists(save_name) ){
     # set of species and samples
@@ -16,26 +16,40 @@ add_missing_zeros = function( data_frame, unique_sample_ID_colname, sample_colna
     unique_data_frame[,sample_colname] = 0
 
     # Loop through species
-    new_data_frame = NULL
-    for(p in 1:length(species_set)){
-      temp_data_frame = unique_data_frame
-      temp_data_frame[,species_colname] = species_set[p]
-      for(i in 1:nrow(temp_data_frame)){               #
-        #Match = which( data_frame[,unique_sample_ID_colname]==unique_sample_ID_set[i] & data_frame[,species_colname]==species_set[p])
-        Match = which( data_frame[,unique_sample_ID_colname]==unique_sample_ID_set[i] )
-        Match = Match[which( data_frame[Match,species_colname]==species_set[p]) ]
-        if( length(Match)>1 ){
-          if( if_multiple_records=="Error") stop( "multiple unique-IDs with catch for same species")
-          if( if_multiple_records=="Combine") temp_data_frame[i,sample_colname] = sum(data_frame[Match,sample_colname])
-          if( if_multiple_records=="First") temp_data_frame[i,sample_colname] = data_frame[Match[1],sample_colname]
+    if( Method=="Slow" ){
+      new_data_frame = NULL
+      for(p in 1:length(species_set)){
+        temp_data_frame = unique_data_frame
+        temp_data_frame[,species_colname] = species_set[p]
+        for(i in 1:nrow(temp_data_frame)){               #
+          #Match = which( data_frame[,unique_sample_ID_colname]==unique_sample_ID_set[i] & data_frame[,species_colname]==species_set[p])
+          Match = which( data_frame[,unique_sample_ID_colname]==unique_sample_ID_set[i] )
+          Match = Match[which( data_frame[Match,species_colname]==species_set[p]) ]
+          if( length(Match)>1 ){
+            if( if_multiple_records=="Error") stop( "multiple unique-IDs with catch for same species")
+            if( if_multiple_records=="Combine") temp_data_frame[i,sample_colname] = sum(data_frame[Match,sample_colname])
+            if( if_multiple_records=="First") temp_data_frame[i,sample_colname] = data_frame[Match[1],sample_colname]
+          }
+          if( length(Match)==1 ){
+            temp_data_frame[i,sample_colname] = data_frame[Match,sample_colname]
+          }
+          if( verbose==TRUE & (i%%1000)==0 ) message( "Processing row ",i," for ",species_set[p] )
         }
-        if( length(Match)==1 ){
-          temp_data_frame[i,sample_colname] = data_frame[Match,sample_colname]
-        }
-        if( verbose==TRUE & (i%%1000)==0 ) message( "Processing row ",i," for ",species_set[p] )
+        new_data_frame = rbind(new_data_frame, temp_data_frame)
+        if( verbose==TRUE ) message( "Finished processing for ",species_set[p] )
       }
-      new_data_frame = rbind(new_data_frame, temp_data_frame)
-      if( verbose==TRUE ) message( "Finished processing for ",species_set[p] )
+    }
+    if( Method=="Fast" ){
+      new_data_frame = NULL
+      for(p in 1:length(species_set)){
+        temp_data_frame = unique_data_frame
+        temp_data_frame[,species_colname] = species_set[p]
+        species_data_frame = data_frame[ which(data_frame[,species_colname]==species_set[p]), ]
+        Match = match( species_data_frame[,unique_sample_ID_colname], temp_data_frame[,unique_sample_ID_colname] )
+        temp_data_frame[Match,sample_colname] = species_data_frame[,sample_colname]
+        new_data_frame = rbind(new_data_frame, temp_data_frame)
+        if( verbose==TRUE ) message( "Finished processing for ",species_set[p] )
+      }
     }
 
     # Relevel
@@ -47,9 +61,10 @@ add_missing_zeros = function( data_frame, unique_sample_ID_colname, sample_colna
     }
 
     # Sanity checks
-    Which = which( data_frame[,species_colname] %in% species_set )
     if( if_multiple_records!="First"){
-      if( sum(new_data_frame[,sample_colname],na.rm=TRUE) != sum(data_frame[Which,sample_colname],na.rm=TRUE) ) stop( "missing rows in new data frame")
+      Which = which( data_frame[,species_colname] %in% species_set )
+      almost_equal = function(a,b,tol=1e-12){ ifelse( abs(a-b)/mean(c(a,b))<tol, TRUE, FALSE ) }
+      if( !almost_equal(sum(new_data_frame[,sample_colname],na.rm=TRUE), sum(data_frame[Which,sample_colname],na.rm=TRUE)) ) stop( "missing rows in new data frame")
     }
 
     # Save
