2c63e82181614710eca03c9638e84c9ea5fcfeca
Jim Thorson <JamesT.esq@gmail.com>
2015-12-20 20:24:19 -0800

    further fixes to Hist_Fn

--- R/Hist_Fn.R
+++ R/Hist_Fn.R
@@ -1,8 +1,8 @@
-Hist_Fn = function( x, freq=TRUE, breaks="Sturges", y_buffer=0.05, ...){
+Hist_Fn = function( x, freq=TRUE, breaks="Sturges", y_buffer=0.05, ylim=NULL, ...){
   Hist = hist( x, breaks=breaks, plot=FALSE )
-  if(freq==TRUE) Ylim = c(0, max(Hist$counts)*(1+y_buffer))
-  if(freq==FALSE) Ylim = c(0, max(Hist$density)*(1+y_buffer))
-  hist( x, breaks=breaks, freq=freq, ylim=Ylim, ...)
-  Return = list("Hist"=Hist, "Ylim"=Ylim)
+  if(is.null(ylim) & freq==TRUE) ylim = c(0, max(Hist$counts)*(1+y_buffer))
+  if(is.null(ylim) & freq==FALSE) ylim = c(0, max(Hist$density)*(1+y_buffer))
+  hist( x, breaks=breaks, freq=freq, ylim=ylim, ...)
+  Return = list("Hist"=Hist, "ylim"=ylim)
   return( invisible(Return) )
 }
