08a272f372ad74e6c229dc8728c48677263dee83
Jim Thorson <JamesT.esq@gmail.com>
2016-05-06 22:49:18 -0700

    small change to Extract_SE

--- R/Extract_SE.R
+++ R/Extract_SE.R
@@ -1,12 +1,15 @@
 
-Extract_SE = function( SD, Dim=NULL, Map=NULL, parname){
-  if( is.null(dim)) Return = rep(NA, sum(parname==rownames(summary(SD))))
-  if( !is.null(dim)) Return = array(NA, dim=Dim)
+Extract_SE = function( SD, Dim=NULL, Map=NULL, parname, columns=2){
+  # Size of return
+  if( is.null(Dim)) Return = array(NA, dim=c(sum(parname==rownames(summary(SD))),length(columns)) )
+  if( !is.null(Dim)) Return = array(NA, dim=c(Dim,length(columns)) )
+  # Fixed or not
   if( !is.null(Map) ){
-    if( parname %in% names(Map) ) Return[which(!is.na(Map[[parname]]))] = summary(SD)[which(parname==rownames(summary(SD))),'Std. Error']
-    if( !(parname %in% names(Map)) ) Return[] = summary(SD)[which(parname==rownames(summary(SD))),'Std. Error']
+    if( parname %in% names(Map) ) Return[which(!is.na(Map[[parname]]))] = summary(SD)[which(parname==rownames(summary(SD))),columns]
+    if( !(parname %in% names(Map)) ) Return[] = summary(SD)[which(parname==rownames(summary(SD))),columns]
   }
-  if( is.null(Map) ) Return[] = summary(SD)[which(parname==rownames(summary(SD))),'Std. Error']
+  if( is.null(Map) ) Return[] = summary(SD)[which(parname==rownames(summary(SD))),columns]
+  # Return things
   return(Return)
 }
 
