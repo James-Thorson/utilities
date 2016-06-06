e5a85f0439058e2105580591ed17992fd9a9136f
Jim Thorson <JamesT.esq@gmail.com>
2016-06-06 09:17:45 -0700

    change Hist_Fn...
    ... to allow plotting of multiple histograms passed via list

--- R/Hist_Fn.R
+++ R/Hist_Fn.R
@@ -1,9 +1,25 @@
-Hist_Fn = function( x, freq=TRUE, breaks="Sturges", y_buffer=0.05, ylim=NULL, main="", col="lightgrey", bty="o", ...){
-  Hist = hist( x, breaks=breaks, plot=FALSE )
-  if(is.null(ylim) & freq==TRUE) ylim = c(0, max(Hist$counts)*(1+y_buffer))
-  if(is.null(ylim) & freq==FALSE) ylim = c(0, max(Hist$density)*(1+y_buffer))
-  hist( x, breaks=breaks, freq=freq, ylim=ylim, col=col, main=main, ...)
+Hist_Fn = function( x, freq=TRUE, breaks="Sturges", y_buffer=0.05, ylim=NULL, main="", col="lightgrey", bty="o", add=FALSE, ...){
+  # Modify default inputs
+  if( is.list(x)==FALSE ) x = list( x )
+  if( length(col)==1 & length(x)>1 ) col = rep(col,length(x))
+
+  # Figure out ylim
+  Hist = NULL
+  if(is.null(ylim)) ylim = c(NA, 0)
+  for(i in 1:length(x)){
+    Hist[[i]] = hist( x[[i]], breaks=breaks, plot=FALSE )
+    if(is.na(ylim[1]) & freq==TRUE) ylim[2] = max(ylim[2], max(Hist[[i]]$counts)*(1+y_buffer) )
+    if(is.na(ylim[1]) & freq==FALSE) ylim[2] = max(ylim[2], max(Hist[[i]]$density)*(1+y_buffer) )
+  }
+  if(is.na(ylim[1])) ylim[1] = 0
+
+  # Plot
+  for(i in 1:length(x)){
+    hist( x[[i]], breaks=breaks, freq=freq, ylim=ylim, col=col[i], main=main, add=ifelse(i==1,add,TRUE), ...)
+  }
   if( bty=="o" ) box()
+
+  # Return stuff
   Return = list("Hist"=Hist, "ylim"=ylim)
   return( invisible(Return) )
 }
