1ceb5c71be2151aaad3b31d212a3b9df445811de
Jim Thorson <JamesT.esq@gmail.com>
2016-06-28 07:28:44 -0700

    small changes

---
+++ R/FormatC.R
@@ -0,0 +1,4 @@
+
+FormatC = function(num, digits=2, format="f"){
+  formatC(num, format=format, digits=digits)
+}
--- R/Hist_Fn.R
+++ R/Hist_Fn.R
@@ -1,4 +1,4 @@
-Hist_Fn = function( x, freq=TRUE, breaks="Sturges", y_buffer=0.05, ylim=NULL, main="", col="lightgrey", bty="o", add=FALSE, ...){
+Hist_Fn = function( x, freq=TRUE, breaks="Sturges", y_buffer=0.05, ylim=NULL, xlim=NULL, main="", col="lightgrey", bty="o", add=FALSE, ...){
   # Modify default inputs
   if( is.list(x)==FALSE ) x = list( x )
   if( length(col)==1 & length(x)>1 ) col = rep(col,length(x))
@@ -6,16 +6,18 @@ Hist_Fn = function( x, freq=TRUE, breaks="Sturges", y_buffer=0.05, ylim=NULL, ma
   # Figure out ylim
   Hist = NULL
   if(is.null(ylim)) ylim = c(NA, 0)
+  if(is.null(xlim)) xlim = c(NA, NA)
   for(i in 1:length(x)){
     Hist[[i]] = hist( x[[i]], breaks=breaks, plot=FALSE )
     if(is.na(ylim[1]) & freq==TRUE) ylim[2] = max(ylim[2], max(Hist[[i]]$counts)*(1+y_buffer) )
     if(is.na(ylim[1]) & freq==FALSE) ylim[2] = max(ylim[2], max(Hist[[i]]$density)*(1+y_buffer) )
+    xlim = range( c(xlim,Hist[[i]]$breaks), na.rm=TRUE)
   }
   if(is.na(ylim[1])) ylim[1] = 0
 
   # Plot
   for(i in 1:length(x)){
-    hist( x[[i]], breaks=breaks, freq=freq, ylim=ylim, col=col[i], main=main, add=ifelse(i==1,add,TRUE), ...)
+    hist( x[[i]], breaks=breaks, freq=freq, ylim=ylim, xlim=xlim, col=col[i], main=main, add=ifelse(i==1,add,TRUE), ...)
   }
   if( bty=="o" ) box()
 
