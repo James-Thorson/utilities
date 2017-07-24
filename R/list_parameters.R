88c57d885adf3dbc0d6d9d4d949f2414b42847d2
Jim Thorson <JamesT.esq@gmail.com>
2017-07-24 11:04:50 -0700

    small change to `list_parameters`...
    .. to optionally turn off printing to terminal

--- R/list_parameters.R
+++ R/list_parameters.R
@@ -8,7 +8,7 @@
 #' @return Return Tagged-list of fixed and random effects (returned invisibly)
 
 #' @export
-list_parameters = function( Obj ){
+list_parameters = function( Obj, verbose=TRUE ){
   Return = list()
   Table = data.frame()
   if( length(Obj$env$random)>0 ){
@@ -20,7 +20,9 @@ list_parameters = function( Obj ){
     Return[["Fixed_effects"]] = names(Obj$env$last.par)
     Table = data.frame("Coefficient_name"=names(table(Return[["Fixed_effects"]])), "Number_of_coefficients"=as.numeric(table(Return[["Fixed_effects"]])), "Type"="Fixed")
   }
-  message("List of estimated fixed and random effects:")
-  print(Table)
+  if( verbose==TRUE ){
+    message("List of estimated fixed and random effects:")
+    print(Table)
+  }
   return( invisible(Table) )
 }
