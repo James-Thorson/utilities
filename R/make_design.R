325d01c4df6eef3bba2b7f7339e684e865106e9b
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2022-02-01 07:27:31 -0800

    Update make_design.R

--- R/make_design.R
+++ R/make_design.R
@@ -19,25 +19,42 @@ make_design <-
 function( formula,
           data,
           random_spline = FALSE,
+          coerce_to_dense = TRUE,
           ... ){
 
   library(splines)
-  f1 = lme4::findbars(formula)
+
+  # Change defaults to allow NAs in data without messing up number of rows
+  na.action.orig = options()$na.action
+  on.exit( options(na.action=na.action.orig) )
+  options(na.action='na.pass')
+
+  # Build fixed effects
   f2 = lme4::nobars(formula)
   X_ij = model.matrix(f2, data)
   Group_j = rep(NA,ncol(X_ij))
+
+  # Dealw with splines
   if( random_spline==TRUE ){
     which_bs = grep( pattern="bs(", x=colnames(X_ij), fixed=TRUE )
     if( length(which_bs) >= 2 ) Group_j[which_bs] = 1
   }
+
+  # Build random effects
+  f1 = lme4::findbars(formula)
   if( length(f1) > 0 ){
-    fr = model.frame(formula=lme4::subbars(formula), data=data, ...)
+    fr = model.frame(formula=lme4::subbars(formula), data=data) #, ...)
     m2 = lme4::mkReTrms(f1, fr=fr)
-    for( i in seq_len(length(m2$Ztlist)) ){
-      Z_ij = Matrix::t(m2$Ztlist[[i]])
+    for( k in seq_len(length(m2$Ztlist)) ){
+      Z_ij = Matrix::t(m2$Ztlist[[k]])
       X_ij = cbind( X_ij, Z_ij )
       Group_j = c( Group_j, rep(max(c(0,Group_j),na.rm=TRUE)+1,ncol(Z_ij)) )
     }
+  }
+
+  # Return
+  if( coerce_to_dense==TRUE ){
+    X_ij = as.matrix(X_ij)
   }
   return(list("X_ij"=X_ij,"Group_j"=Group_j))
 }
